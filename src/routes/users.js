23f9862785e497110b6623042ae3c314e2b1c544
Patrick Aparecido <afs.patrick@gmail.com>
2020-03-12 22:15:25 -0300

    minor adjustments in the some routes e creation of a middleware for some data integration/validation

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,10 +6,13 @@ router.get('/', controller.getAll)
 
 router.get('/:userId', controller.getById)
 
-router.post('/', controller.create)
+router.post('/signup', controller.create)
 
 router.put('/:userId', controller.update)
 
 router.delete('/:userId', controller.delete)
+
+//operacoes de login
+router.post('/login', controller.login)
 
 module.exports = router
