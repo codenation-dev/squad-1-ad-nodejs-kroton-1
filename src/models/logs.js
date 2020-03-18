2c88998b0d9a7f4b708c064fd50a8f7b5459adc4
Patrick Aparecido <afs.patrick@gmail.com>
2020-03-17 22:58:28 -0300

    Primeiras linhas de desenvolvimento.

--- src/models/logs.js
+++ src/models/logs.js
@@ -1,4 +1,14 @@
 module.exports = (sequelize, DataTypes) =>
   sequelize.define('logs', {
     // ... Implemente as colunas de logs
+    //testando commit
+    level: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: DataTypes.STRING,
+    message: DataTypes.STRING,
+    //
+    events: DataTypes.INTEGER,
+    archived: DataTypes.INTEGER
   })
--- src/models/users.js
+++ src/models/users.js
@@ -1,4 +1,13 @@
 module.exports = (sequelize, DataTypes) =>
   sequelize.define('users', {
     // ... Implemente as colunas de users
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    token: DataTypes.STRING
   })
