2dc84134de2ef2ad63465e4016749ddb9fd0425a
Patrick Aparecido <afs.patrick@gmail.com>
2020-03-21 11:51:17 -0300

    implement and equalize of all models

--- src/models/logs.js
+++ src/models/logs.js
@@ -1,14 +1,15 @@
 module.exports = (sequelize, DataTypes) =>
   sequelize.define('logs', {
-    // ... Implemente as colunas de logs
-    //testando commit
     level: {
       type: DataTypes.STRING,
       allowNull: false
     },
     description: DataTypes.STRING,
-    message: DataTypes.STRING,
-    //
-    events: DataTypes.INTEGER,
-    archived: DataTypes.INTEGER
+    message: DataTypes.STRING,    
+    system: {
+      type: DataTypes.STRING,
+      validate: {
+        isLowerCase: true
+      }
+    }
   })
--- src/models/users.js
+++ src/models/users.js
@@ -1,13 +1,18 @@
 module.exports = (sequelize, DataTypes) =>
-  sequelize.define('users', {
-    // ... Implemente as colunas de users
-    email: {
+  sequelize.define('users', {  
+    name: {
       type: DataTypes.STRING,
       allowNull: false
+    },  
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    token: DataTypes.STRING
+    }
   })
